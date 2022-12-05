08ff5f7bc521639700f1cfa184b8f95d41e2011a
07125 <07125@skgas.co.kr>
2022-12-05 17:49:53 +0900

    테스트

--- openpyxl_test/openpyxl_test.py
+++ openpyxl_test/openpyxl_test.py
@@ -1,26 +1,21 @@
 
 import openpyxl
  
-# �������� ����
 wb = openpyxl.load_workbook('score.xlsx')
  
-# ���� Active Sheet ���
 ws = wb.active
 # ws = wb.get_sheet_by_name("Sheet1")
  
-# ������ ������ �б�
 for r in ws.rows:
-    row_index = r[0].row   # �� �ε���
+    row_index = r[0].row   
     kor = r[1].value
     eng = r[2].value
     math = r[3].value
     sum = kor + eng + math
  
-    # �հ� ����
     ws.cell(row=row_index, column=5).value = sum
     
     print(kor, eng, math, sum)
- 
-# ���� ���� ����
+
 wb.save("score2.xlsx")
 wb.close()
